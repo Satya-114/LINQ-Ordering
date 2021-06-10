879a6d3ab0df28f206e42b52e00fef2f044b34bb
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:55:03 +0530

    Add files via upload

---
+++ ThenBy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_ThenBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Method Syntax
+            var MS = Student.GetAllStudents()
+                              .OrderBy(x => x.FirstName)
+                              .ThenBy(y => y.LastName)
+                              .ToList();
+
+            foreach (var student in MS)
+            {
+                Console.WriteLine("First Name :" + student.FirstName + ", Last Name : " + student.LastName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
