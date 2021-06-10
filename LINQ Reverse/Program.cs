48c4bb1fe79648c608ee77eb81851990c2f528bd
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:58:54 +0530

    Add files via upload

---
+++ LINQ Reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[] { 10, 30, 50, 40, 60, 20, 70, 100 };
+            Console.WriteLine("Before Reverse the Data");
+            foreach (var number in intArray)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            IEnumerable<int> ArrayReversedData = intArray.Reverse();
+            Console.WriteLine("After Reverse the Data");
+            foreach (var number in ArrayReversedData)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
