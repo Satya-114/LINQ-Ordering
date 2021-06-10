e8b3ac9520751d46faef072a0cfd76fb4d9db59a
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:56:52 +0530

    Add files via upload

---
+++ ThenBy Descending/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThenBy_Descending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method Syntax
+            var MS = Student.GetAllStudents()
+                              .OrderBy(x => x.FirstName)
+                              .ThenByDescending(y => y.LastName)
+                              .ToList();
+
+            foreach (var student in MS)
+            {
+                Console.WriteLine("First Name :" + student.FirstName + ", Last Name : " + student.LastName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
