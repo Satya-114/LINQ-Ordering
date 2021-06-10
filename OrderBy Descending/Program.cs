78e206cf647a148348160a0ffe9a35b08eb16c1d
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:52:01 +0530

    Add files via upload

---
+++ OrderBy Descending/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderBy_Descending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() { 10, 45, 35, 29, 100, 69, 58, 50 };
+            Console.WriteLine("Before Sorting the Data: ");
+            foreach (var item in intList)
+            {
+                Console.Write(item + " ");
+            }
+            //Sorting the data in Descending Order
+            //Using Method Syntax
+            var MS = intList.OrderByDescending(num => num);
+            //Using Query Syntax
+            var QS = (from num in intList
+                      orderby num descending
+                      select num).ToList();
+            Console.WriteLine();
+            Console.WriteLine("After Sorting the Data in Descending Order: ");
+            foreach (var item in QS)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
